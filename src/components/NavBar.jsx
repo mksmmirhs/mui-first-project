8a5779909e64ca54d0834b291c55613c485c7e91
mksmmirhs <meraj.exe@gmail.com>
2024-05-15 19:26:39 +0600

    logo done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,36 @@
+import { DeveloperBoard } from "@mui/icons-material";
+import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  const StyledToolBar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+  return (
+    <AppBar position="sticky">
+      <StyledToolBar>
+        <Typography
+          variant="h6"
+          sx={{
+            display: {
+              xs: "none",
+              sm: "block",
+            },
+          }}
+        >
+          MUI dev
+        </Typography>
+        <DeveloperBoard
+          sx={{
+            display: {
+              xs: "block",
+              sm: "none",
+            },
+          }}
+        />
+      </StyledToolBar>
+    </AppBar>
+  );
 }
 
 export default NavBar;
