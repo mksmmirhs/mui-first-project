6f76e8e0c1a3aaa9bcaa433abf532240064b53b7
mksmmirhs <meraj.exe@gmail.com>
2024-05-16 12:39:29 +0600

    menu done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,16 @@ import {
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 
 function NavBar() {
+  const [open, setOpen] = useState(false);
   const StyledToolBar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
@@ -24,9 +28,21 @@ function NavBar() {
   }));
 
   const Icons = styled(Box)(({ theme }) => ({
-    display: "flex",
+    display: "none",
     alignItems: "center",
     gap: "20px",
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+  }));
+
+  const UserBox = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+    },
+    display: "none",
+    alignItems: "center",
+    gap: "10px",
   }));
 
   return (
@@ -67,6 +83,9 @@ function NavBar() {
             <Notifications />
           </Badge>
           <Avatar
+            onClick={(e) => {
+              setOpen(true);
+            }}
             sx={{
               height: 30,
               width: 30,
@@ -74,7 +93,40 @@ function NavBar() {
             src="https://img.freepik.com/free-photo/close-up-portrait-curly-handsome-european-male_176532-8133.jpg?t=st=1715793785~exp=1715797385~hmac=5fa2020d2d062ff5c0cf10df78084e851090d905f9d0847988bd8cc7614b9eac&w=1060"
           ></Avatar>
         </Icons>
+        <UserBox>
+          <Avatar
+            onClick={(e) => {
+              setOpen(true);
+            }}
+            sx={{
+              height: 30,
+              width: 30,
+            }}
+            src="https://img.freepik.com/free-photo/close-up-portrait-curly-handsome-european-male_176532-8133.jpg?t=st=1715793785~exp=1715797385~hmac=5fa2020d2d062ff5c0cf10df78084e851090d905f9d0847988bd8cc7614b9eac&w=1060"
+          ></Avatar>
+          <Typography variant="span">Sam</Typography>
+        </UserBox>
       </StyledToolBar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => {
+          setOpen(false);
+        }}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 }
