826917047ed445701e6b09ecbd364d03efc53ecc
mksmmirhs <meraj.exe@gmail.com>
2024-05-16 12:08:22 +0600

    avatar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,34 @@
-import { DeveloperBoard } from "@mui/icons-material";
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import { DeveloperBoard, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 function NavBar() {
   const StyledToolBar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
   });
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+  }));
+
+  const Icons = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "20px",
+  }));
+
   return (
     <AppBar position="sticky">
       <StyledToolBar>
@@ -28,6 +51,29 @@ function NavBar() {
             },
           }}
         />
+        <Search>
+          <InputBase
+            sx={{
+              width: "100%",
+            }}
+            placeholder="Search..."
+          ></InputBase>
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{
+              height: 30,
+              width: 30,
+            }}
+            src="https://img.freepik.com/free-photo/close-up-portrait-curly-handsome-european-male_176532-8133.jpg?t=st=1715793785~exp=1715797385~hmac=5fa2020d2d062ff5c0cf10df78084e851090d905f9d0847988bd8cc7614b9eac&w=1060"
+          ></Avatar>
+        </Icons>
       </StyledToolBar>
     </AppBar>
   );
